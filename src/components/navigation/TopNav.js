1f8bdf51eaf3fdfceb30e6e0cce33762d8c608f7
Colin Schlecht <schlechtcolin@yahoo.com>
2021-06-12 15:30:43 -0700

    adds topnav

---
+++ src/components/navigation/TopNav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const TopNav = () => {
+
+    const onLogoutClick = () => {
+        onLogout();
+      };
+  return (
+    <div className="ui menu secondary">
+      <Link to="/dashboard" className="item">
+        Swolemates
+      </Link>
+      <Link to="/" className="item">
+        link to something
+      </Link>
+      <div className="right menu">
+        <Link className="item">Sign Up</Link>
+        <Link className="item">Help</Link>
+      </div>
+    </div>
+  );
+};
+
+export default TopNav;
